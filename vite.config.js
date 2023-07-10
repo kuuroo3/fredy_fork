f71484eec7581555a24e58f1f93c88aaa2b1dc36
tessa <tessaberg1305@gmail.com>
2023-07-10 23:21:45 +0200

    wip: changes for pi

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
-  build: {
-    chunkSizeWarningLimit: 9999999,
-    outDir: './ui/public',
-  },
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: {
-          host: 'https://tessaberg.de/fredy',
-          port: 9998,
-          changeOrigin: true,
-          secure: false
-        },
-      },
+    base: '',
+    build: {
+        chunkSizeWarningLimit: 9999999,
+        outDir: './ui/public',
     },
-  },
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://tessaberg.de/fredy/',
+                changeOrigin: true,
+                // rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 });
